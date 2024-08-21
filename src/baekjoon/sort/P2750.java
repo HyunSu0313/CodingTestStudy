b462aebe4b9318b416c3a5acf6df2b68386d8e84
jhs99126 <jhs99126@naver.com>
2024-08-21 19:38:48 +0900

    sort

---
+++ src/baekjoon/sort/P2750.java
@@ -0,0 +1,34 @@
+package baekjoon.sort;
+
+import java.util.*;
+
+/**
+ * 백준 2750 (버블 정렬 기초)
+ */
+public class P2750 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}

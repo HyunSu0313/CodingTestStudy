0b8118c1728bd7c0429508c261b9211412e06760
jhs99126 <jhs99126@naver.com>
2024-08-22 00:05:05 +0900

    Stack&Queue

--- src/programmers/stackandqueue/P12909.java
+++ src/programmers/stackandqueue/P12909.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class P12909 {
 
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
                 stack.push('(');

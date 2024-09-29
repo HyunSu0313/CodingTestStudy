fc4d1d361a8896a2c26b4dc1718a4434b45d1ce7
jhs99126 <jhs99126@naver.com>
2024-09-29 15:05:43 +0900

    Tree

--- src/baekjoon/tree/P10868.java
+++ src/baekjoon/tree/P10868.java
@@ -3,31 +3,74 @@ package baekjoon.tree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 백준 10868 세그먼트 트리
+ * 최솟값 구하기
+ */
 public class P10868 {
     static long[] tree;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int length = n;
-        int treeDepth = 0;
+        int treeHeight = 0;
+        int lenght = N;
 
-        while (length != 0) {
-            length /= 2;
-            treeDepth++;
+        while (lenght != 0) {
+            lenght /= 2;
+            treeHeight++;
         }
 
-        int arrayLength = (int) Math.pow(2, treeDepth) * 2;
-        int leftNodeStart = arrayLength - 1;
+        int treeSize = (int) Math.pow(2, treeHeight + 1);
+        int leftNodeStartIndex = treeSize / 2 - 1;
 
-        tree = new long[arrayLength + 1];
+        tree = new long[treeSize + 1];
+        Arrays.fill(tree, Long.MAX_VALUE);
 
-        
+        for (int i = leftNodeStartIndex + 1; i <= leftNodeStartIndex + N; i++) {
+            tree[i] = Long.parseLong(br.readLine());
+        }
+
+        setTree(treeSize - 1);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            s = s + leftNodeStartIndex;
+            e = e + leftNodeStartIndex;
+            System.out.println(getMin(s, e));
+        }
+        br.close();
+    }
+
+    private static long getMin(int s, int e) {
+        long Min = Long.MAX_VALUE;
+        while (s <= e) {
+            if (s % 2 == 1) {
+                Min = Math.min(Min, tree[s]);
+                s++;
+            }
+            s = s / 2;
+            if (e % 2 == 0) {
+                Min = Math.min(Min, tree[e]);
+                e--;
+            }
+            e = e / 2;
+        }
+        return Min;
+    }
+
+    private static void setTree(int i) {
+        while (i != 1) {
+            if (tree[i / 2] > tree[i])
+                tree[i / 2] = tree[i];
+            i--;
+        }
     }
 }
